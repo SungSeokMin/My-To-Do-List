1a78aff3bdeab1d5da82f9273e939a8dc45bb8ec
SungSeokMin <jkl154527@gmail.com>
2020-11-15 15:43:59 +0900

    2020-11-15 2차 커밋

--- README.md
+++ README.md
@@ -10,3 +10,9 @@
    2-1. 이름과 메세지에 값이 없으면 등록을 하지 못하게 한다.
    2-2. 한번 등록하고 나면 User와 Message의 값은 사라진다.
 3. checkbox를 클릭하면 background-color가 변경된다. (완료를 표시)
+
+   2020.11.15(일)
+
+4. chechbox를 클릭하면 -> classList.add('complete');
+5. checkbox를 다시 클릭하면 -> classList.remove('complete');
+6. 완료 버튼을 누르면 <li>태그가 삭제되면서 화면에서 사라진다.
--- script.js
+++ script.js
@@ -5,10 +5,15 @@ const messageInfo = inputInfo.querySelectorAll('input')[1];
 
 const btn = document.querySelector('#btn');
 
-function makeMyList (userName, userMessage) {
+function createUl () {
   const totalList = document.querySelector('#content');
-  
   const ulElement = document.createElement('ul');
+  totalList.appendChild(ulElement);
+}
+
+function makeMyList (userName, userMessage) {
+
+  const ulElement = document.querySelector('ul');
 
   const liElement = document.createElement('li');
   liElement.classList.add('list')
@@ -27,12 +32,16 @@ function makeMyList (userName, userMessage) {
 
   const checkList = document.createElement('input');
   checkList.setAttribute('type', 'checkbox')
-  checkList.textContent = '완료';
   checkList.onclick = changeBackgroundHandler;
 
-  liElement.append(user, message, createdAt, checkList);
+  const button = document.createElement('button');
+  button.classList.add('completeBtn');
+  button.textContent = '완료';
+  button.onclick = checkRemoveListHandler;
+
+  liElement.append(user, message, createdAt, checkList, button);
   ulElement.append(liElement);
-  totalList.appendChild(ulElement);
+  // totalList.appendChild(ulElement);
 }
 
 //등록버튼 클릭 후 DATA에 저장
@@ -58,14 +67,24 @@ function inputToDoListHandler (e) {
 }
 
 function changeBackgroundHandler (e) {
-  let parent = e.target.parentElement;
-  let checkComplete = document.querySelector('.complete');
-  const liElement = document.querySelectorAll('li');
 
-  for(let i = 0; i < liElement.length; i++) {
-    if(parent) {
-      parent.classList.add('complete');  
-    }
+  let parent = e.target.parentElement;
+
+  // checked가 false -> li에 complete 클래스를 remove
+  // checked가 true -> li에 complete 클래스를 add
+
+  if(this.checked) {
+    parent.classList.add('complete');
+  } else {
+    parent.classList.remove('complete');
   }
 }
+
+function checkRemoveListHandler (e) {
+  //버튼 클릭 시 ul 태그를 remove.
+  let parent = e.target.parentElement;
+  parent.remove();
+}
+
 btn.addEventListener('click', inputToDoListHandler);
+window.onload = createUl;
